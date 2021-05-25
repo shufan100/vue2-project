266560eb730baa3d1169a54d5ae904ea72683872
“shufan100” <1549248097@qq.com>
2021-05-25 17:43:20 +0800

    删除svg

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
+  // root: true,
+  // env: {
+  //   node: true
+  // },
   extends: ["plugin:vue/essential", "@vue/standard"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "space-before-function-paren": 0
-  }
+  // parserOptions: {
+  //   parser: "babel-eslint"
+  // },
+  // rules: {
+  //   "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+  //   "no-": process.env.NODE_ENV === "production" ? "warn" : "off",
+  //   "space-before-function-paren": 0
+  // }
 };
