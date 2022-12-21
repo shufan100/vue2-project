71b926e30f9dda48060d62823480ed069c8da79e
shufan100 <1549248097@qq.com>
2022-12-21 15:29:26 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: shufan100 1549248097@qq.com
  * @Date: 2022-10-23 14:45:51
  * @LastEditors: shufan100 1549248097@qq.com
- * @LastEditTime: 2022-12-05 11:07:22
+ * @LastEditTime: 2022-12-21 15:28:58
  * @FilePath: \vue2-project\vue.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
